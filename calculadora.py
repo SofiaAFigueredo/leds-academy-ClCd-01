8cdd892ae6774f5b582ce11ace568fca26bb2a1b
SofiaAFigueredo <sofiaalvesfigueredo@gmail.com>
2026-06-10 11:09:14 -0300

    Inicial

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+def somar(a, b):
+    """Função que recebe dois números e retorna a soma deles."""
+    return a + b
+
+def subtrair(a, b):
+    """Função que recebe dois números e retorna a subtração deles."""
+    return a - b
